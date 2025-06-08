123fa652944b637dbaa51ddbd3eb490d9619c685
Jon Stuebe <jstuebe@gmail.com>
2025-06-08 13:10:23 -0700

    feat: removed unused code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,14 @@
 import { ThemeProvider } from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { theme } from "react-native-orchard/theme";
-import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
+import "react-native-reanimated";
 
 export default function RootLayout() {
   const mode = useColorScheme() ?? "light";
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
     <GestureHandlerRootView>
       <ThemeProvider value={theme[mode]}>
